0cb3ca3620c224c51465f781c38fab3c4a8fe6d3
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-01-08 02:52:35 +0100

    Added the urls and corresponding views for CRUD

--- mywebsite/blog/views.py
+++ mywebsite/blog/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
 
+from .models import Post, Comment
+
+from .forms import PostForm, CommentForm 
+
+from django.urls import reverse_lazy
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.views.generic import (TemplateView,
+ListView, DetailView, CreateView, UpdateView,
+DeleteView)
+
 # Create your views here.
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        # __lte == less than or equal to
+        return Post.objects.filter(published_date__lte=timezone.now()).oreder_by('-published_date') 
+
+class PostDetailView(DetailView):
+    model = Post
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostUpdateView(UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).oreder_by('created_date')
